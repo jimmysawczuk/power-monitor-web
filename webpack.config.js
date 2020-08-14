13491aebee6b5cd6ab77f0a641fd23a607b0da47
Jimmy Sawczuk <me@jimmysawczuk.com>
2020-08-14 16:39:09 -0400

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,67 @@
+const path = require("path")
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+const TerserJSPlugin = require("terser-webpack-plugin")
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin")
+
+const config = {
+  plugins: [new MiniCssExtractPlugin()],
+  optimization: {
+    minimizer: [new TerserJSPlugin({}), new OptimizeCSSAssetsPlugin({})],
+  },
+  entry: {
+    main: "./src/main.js",
+  },
+  output: {
+    path: path.resolve(__dirname, "public/dist"),
+    filename: "[name].js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: [
+          {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env"],
+              plugins: ["@babel/plugin-proposal-object-rest-spread"],
+            },
+          },
+        ],
+      },
+      {
+        test: /\.less$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          { loader: "css-loader" },
+          { loader: "postcss-loader" },
+          { loader: "less-loader" },
+        ],
+      },
+      {
+        test: /\.(svg|woff|woff2|eot|ttf)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "type/[name].[sha256:contenthash:hex:8].[ext]",
+            },
+          },
+        ],
+      },
+    ],
+  },
+}
+
+module.exports = (env, argv) => {
+  if (argv.mode === "development") {
+    delete config.optimization
+
+    config.devtool = "eval"
+  }
+
+  return config
+}
